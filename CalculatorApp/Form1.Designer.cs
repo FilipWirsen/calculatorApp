bffd260f4565ad14805b7102242a4031e54ed5d1
Filip Wirsén <filip.wirsen99@gmail.com>
2022-10-24 00:20:05 +0200

    Initial commit

---
+++ CalculatorApp/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace CalculatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.zeroButton = new System.Windows.Forms.Button();
+            this.plusMinusButton = new System.Windows.Forms.Button();
+            this.decimalButton = new System.Windows.Forms.Button();
+            this.equalButton = new System.Windows.Forms.Button();
+            this.plusButton = new System.Windows.Forms.Button();
+            this.threeButton = new System.Windows.Forms.Button();
+            this.oneButton = new System.Windows.Forms.Button();
+            this.twoButton = new System.Windows.Forms.Button();
+            this.minusButton = new System.Windows.Forms.Button();
+            this.sixButton = new System.Windows.Forms.Button();
+            this.fourButton = new System.Windows.Forms.Button();
+            this.fiveButton = new System.Windows.Forms.Button();
+            this.divideButton = new System.Windows.Forms.Button();
+            this.nineButton = new System.Windows.Forms.Button();
+            this.sevenButton = new System.Windows.Forms.Button();
+            this.eightButton = new System.Windows.Forms.Button();
+            this.multiplyButton = new System.Windows.Forms.Button();
+            this.backspaceButton = new System.Windows.Forms.Button();
+            this.squareRootButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // zeroButton
+            // 
+            this.zeroButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.zeroButton.Location = new System.Drawing.Point(174, 669);
+            this.zeroButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.zeroButton.Name = "zeroButton";
+            this.zeroButton.Padding = new System.Windows.Forms.Padding(1);
+            this.zeroButton.Size = new System.Drawing.Size(158, 114);
+            this.zeroButton.TabIndex = 0;
+            this.zeroButton.Text = "0";
+            this.zeroButton.UseVisualStyleBackColor = true;
+            // 
+            // plusMinusButton
+            // 
+            this.plusMinusButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.plusMinusButton.Location = new System.Drawing.Point(10, 669);
+            this.plusMinusButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.plusMinusButton.Name = "plusMinusButton";
+            this.plusMinusButton.Padding = new System.Windows.Forms.Padding(1);
+            this.plusMinusButton.Size = new System.Drawing.Size(158, 114);
+            this.plusMinusButton.TabIndex = 1;
+            this.plusMinusButton.Text = "+/-";
+            this.plusMinusButton.UseVisualStyleBackColor = true;
+            // 
+            // decimalButton
+            // 
+            this.decimalButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.decimalButton.Location = new System.Drawing.Point(338, 669);
+            this.decimalButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.decimalButton.Name = "decimalButton";
+            this.decimalButton.Padding = new System.Windows.Forms.Padding(1);
+            this.decimalButton.Size = new System.Drawing.Size(158, 114);
+            this.decimalButton.TabIndex = 2;
+            this.decimalButton.Text = ".";
+            this.decimalButton.UseVisualStyleBackColor = true;
+            // 
+            // equalButton
+            // 
+            this.equalButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.equalButton.Location = new System.Drawing.Point(502, 669);
+            this.equalButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.equalButton.Name = "equalButton";
+            this.equalButton.Padding = new System.Windows.Forms.Padding(1);
+            this.equalButton.Size = new System.Drawing.Size(158, 114);
+            this.equalButton.TabIndex = 3;
+            this.equalButton.Text = "=";
+            this.equalButton.UseVisualStyleBackColor = true;
+            // 
+            // plusButton
+            // 
+            this.plusButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.plusButton.Location = new System.Drawing.Point(502, 549);
+            this.plusButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.plusButton.Name = "plusButton";
+            this.plusButton.Padding = new System.Windows.Forms.Padding(1);
+            this.plusButton.Size = new System.Drawing.Size(158, 114);
+            this.plusButton.TabIndex = 7;
+            this.plusButton.Text = "+";
+            this.plusButton.UseVisualStyleBackColor = true;
+            // 
+            // threeButton
+            // 
+            this.threeButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.threeButton.Location = new System.Drawing.Point(338, 549);
+            this.threeButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.threeButton.Name = "threeButton";
+            this.threeButton.Padding = new System.Windows.Forms.Padding(1);
+            this.threeButton.Size = new System.Drawing.Size(158, 114);
+            this.threeButton.TabIndex = 6;
+            this.threeButton.Text = "3";
+            this.threeButton.UseVisualStyleBackColor = true;
+            // 
+            // oneButton
+            // 
+            this.oneButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.oneButton.Location = new System.Drawing.Point(10, 549);
+            this.oneButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.oneButton.Name = "oneButton";
+            this.oneButton.Padding = new System.Windows.Forms.Padding(1);
+            this.oneButton.Size = new System.Drawing.Size(158, 114);
+            this.oneButton.TabIndex = 5;
+            this.oneButton.Text = "1";
+            this.oneButton.UseVisualStyleBackColor = true;
+            // 
+            // twoButton
+            // 
+            this.twoButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.twoButton.Location = new System.Drawing.Point(174, 549);
+            this.twoButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.twoButton.Name = "twoButton";
+            this.twoButton.Padding = new System.Windows.Forms.Padding(1);
+            this.twoButton.Size = new System.Drawing.Size(158, 114);
+            this.twoButton.TabIndex = 4;
+            this.twoButton.Text = "2";
+            this.twoButton.UseVisualStyleBackColor = true;
+            // 
+            // minusButton
+            // 
+            this.minusButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.minusButton.Location = new System.Drawing.Point(502, 429);
+            this.minusButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.minusButton.Name = "minusButton";
+            this.minusButton.Padding = new System.Windows.Forms.Padding(1);
+            this.minusButton.Size = new System.Drawing.Size(158, 114);
+            this.minusButton.TabIndex = 11;
+            this.minusButton.Text = "-";
+            this.minusButton.UseVisualStyleBackColor = true;
+            // 
+            // sixButton
+            // 
+            this.sixButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.sixButton.Location = new System.Drawing.Point(338, 429);
+            this.sixButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.sixButton.Name = "sixButton";
+            this.sixButton.Padding = new System.Windows.Forms.Padding(1);
+            this.sixButton.Size = new System.Drawing.Size(158, 114);
+            this.sixButton.TabIndex = 10;
+            this.sixButton.Text = "6";
+            this.sixButton.UseVisualStyleBackColor = true;
+            // 
+            // fourButton
+            // 
+            this.fourButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.fourButton.Location = new System.Drawing.Point(10, 429);
+            this.fourButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.fourButton.Name = "fourButton";
+            this.fourButton.Padding = new System.Windows.Forms.Padding(1);
+            this.fourButton.Size = new System.Drawing.Size(158, 114);
+            this.fourButton.TabIndex = 9;
+            this.fourButton.Text = "4";
+            this.fourButton.UseVisualStyleBackColor = true;
+            // 
+            // fiveButton
+            // 
+            this.fiveButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.fiveButton.Location = new System.Drawing.Point(174, 429);
+            this.fiveButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.fiveButton.Name = "fiveButton";
+            this.fiveButton.Padding = new System.Windows.Forms.Padding(1);
+            this.fiveButton.Size = new System.Drawing.Size(158, 114);
+            this.fiveButton.TabIndex = 8;
+            this.fiveButton.Text = "5";
+            this.fiveButton.UseVisualStyleBackColor = true;
+            // 
+            // divideButton
+            // 
+            this.divideButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.divideButton.Location = new System.Drawing.Point(502, 309);
+            this.divideButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.divideButton.Name = "divideButton";
+            this.divideButton.Padding = new System.Windows.Forms.Padding(1);
+            this.divideButton.Size = new System.Drawing.Size(158, 114);
+            this.divideButton.TabIndex = 15;
+            this.divideButton.Text = "÷";
+            this.divideButton.UseVisualStyleBackColor = true;
+            // 
+            // nineButton
+            // 
+            this.nineButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.nineButton.Location = new System.Drawing.Point(338, 309);
+            this.nineButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.nineButton.Name = "nineButton";
+            this.nineButton.Padding = new System.Windows.Forms.Padding(1);
+            this.nineButton.Size = new System.Drawing.Size(158, 114);
+            this.nineButton.TabIndex = 14;
+            this.nineButton.Text = "9";
+            this.nineButton.UseVisualStyleBackColor = true;
+            // 
+            // sevenButton
+            // 
+            this.sevenButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.sevenButton.Location = new System.Drawing.Point(10, 309);
+            this.sevenButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.sevenButton.Name = "sevenButton";
+            this.sevenButton.Padding = new System.Windows.Forms.Padding(1);
+            this.sevenButton.Size = new System.Drawing.Size(158, 114);
+            this.sevenButton.TabIndex = 13;
+            this.sevenButton.Text = "7";
+            this.sevenButton.UseVisualStyleBackColor = true;
+            // 
+            // eightButton
+            // 
+            this.eightButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.eightButton.Location = new System.Drawing.Point(174, 309);
+            this.eightButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.eightButton.Name = "eightButton";
+            this.eightButton.Padding = new System.Windows.Forms.Padding(1);
+            this.eightButton.Size = new System.Drawing.Size(158, 114);
+            this.eightButton.TabIndex = 12;
+            this.eightButton.Text = "8";
+            this.eightButton.UseVisualStyleBackColor = true;
+            // 
+            // multiplyButton
+            // 
+            this.multiplyButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.multiplyButton.Location = new System.Drawing.Point(502, 189);
+            this.multiplyButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.multiplyButton.Name = "multiplyButton";
+            this.multiplyButton.Padding = new System.Windows.Forms.Padding(1);
+            this.multiplyButton.Size = new System.Drawing.Size(158, 114);
+            this.multiplyButton.TabIndex = 19;
+            this.multiplyButton.Text = "X";
+            this.multiplyButton.UseVisualStyleBackColor = true;
+            // 
+            // backspaceButton
+            // 
+            this.backspaceButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.backspaceButton.Location = new System.Drawing.Point(338, 189);
+            this.backspaceButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.backspaceButton.Name = "backspaceButton";
+            this.backspaceButton.Padding = new System.Windows.Forms.Padding(1);
+            this.backspaceButton.Size = new System.Drawing.Size(158, 114);
+            this.backspaceButton.TabIndex = 18;
+            this.backspaceButton.Text = "←";
+            this.backspaceButton.UseVisualStyleBackColor = true;
+            // 
+            // squareRootButton
+            // 
+            this.squareRootButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.squareRootButton.Location = new System.Drawing.Point(10, 189);
+            this.squareRootButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.squareRootButton.Name = "squareRootButton";
+            this.squareRootButton.Padding = new System.Windows.Forms.Padding(1);
+            this.squareRootButton.Size = new System.Drawing.Size(158, 114);
+            this.squareRootButton.TabIndex = 17;
+            this.squareRootButton.Text = "√x";
+            this.squareRootButton.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.clearButton.Location = new System.Drawing.Point(174, 189);
+            this.clearButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Padding = new System.Windows.Forms.Padding(1);
+            this.clearButton.Size = new System.Drawing.Size(158, 114);
+            this.clearButton.TabIndex = 16;
+            this.clearButton.Text = "C";
+            this.clearButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 795);
+            this.Controls.Add(this.multiplyButton);
+            this.Controls.Add(this.backspaceButton);
+            this.Controls.Add(this.squareRootButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.divideButton);
+            this.Controls.Add(this.nineButton);
+            this.Controls.Add(this.sevenButton);
+            this.Controls.Add(this.eightButton);
+            this.Controls.Add(this.minusButton);
+            this.Controls.Add(this.sixButton);
+            this.Controls.Add(this.fourButton);
+            this.Controls.Add(this.fiveButton);
+            this.Controls.Add(this.plusButton);
+            this.Controls.Add(this.threeButton);
+            this.Controls.Add(this.oneButton);
+            this.Controls.Add(this.twoButton);
+            this.Controls.Add(this.equalButton);
+            this.Controls.Add(this.decimalButton);
+            this.Controls.Add(this.plusMinusButton);
+            this.Controls.Add(this.zeroButton);
+            this.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MinimumSize = new System.Drawing.Size(688, 834);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button zeroButton;
+        private Button plusMinusButton;
+        private Button decimalButton;
+        private Button equalButton;
+        private Button plusButton;
+        private Button threeButton;
+        private Button oneButton;
+        private Button twoButton;
+        private Button minusButton;
+        private Button sixButton;
+        private Button fourButton;
+        private Button fiveButton;
+        private Button divideButton;
+        private Button nineButton;
+        private Button sevenButton;
+        private Button eightButton;
+        private Button multiplyButton;
+        private Button backspaceButton;
+        private Button squareRootButton;
+        private Button clearButton;
+    }
+}
